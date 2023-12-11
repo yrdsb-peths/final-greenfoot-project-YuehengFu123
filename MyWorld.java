9e211393c119637e29bab5d0952152419291df5e
YuehengFu123 <150176258+YuehengFu123@users.noreply.github.com>
2023-12-11 13:16:14 -0500

    Main world update

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Main world
+ * Yueheng Fu
+ * December 2023
  */
 public class MyWorld extends World
 {
