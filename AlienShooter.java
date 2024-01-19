86222abec4390330ce1621d3d6331a15cb3bb9af
YuehengFu123 <150176258+YuehengFu123@users.noreply.github.com>
2024-01-19 12:30:24 -0500

    Update AlienShooter.java

--- AlienShooter.java
+++ AlienShooter.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class AlienShooter here.
+ * Alien that shoots bullets
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Yueheng Fu
+ * @January 2024
  */
 public class AlienShooter extends Actor
 {
