3add2ec47d789377c03ae272294c2ba22ff3ffa2
YuehengFu123 <150176258+YuehengFu123@users.noreply.github.com>
2024-01-19 12:29:54 -0500

    Update EndScreen.java

--- EndScreen.java
+++ EndScreen.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class EndScreen here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Yueheng Fu
+ * @ January 2024
  */
 public class EndScreen extends World
 {
